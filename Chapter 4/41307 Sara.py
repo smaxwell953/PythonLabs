6469f0f932222752c6b681947467ff9b2ca09435
Sara Maxwell <saraann.maxwell@gmail.com>
2020-05-25 21:07:51 -0600

    Days in month

---
+++ Chapter 4/41307 Sara.py
@@ -0,0 +1,31 @@
+def isYearLeap(year):
+    if (year % 400 == 0):
+        return True
+    elif (year % 100 == 0):
+        return False
+    elif (year % 4 == 0):
+        return True
+    else:
+        return False
+
+def daysInMonth(year, month):
+    if year < 1582 or month < 1 or month > 12:
+        return None
+    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    result = days[month-1]
+    if month == 2 and isYearLeap(year):
+        return 29
+    return result
+
+testYears = [1900, 2000, 2016, 1987]
+testMonths = [2, 2, 1, 11]
+testResults = [28, 29, 31, 30]
+for i in range(len(testYears)):
+	yr = testYears[i]
+	mo = testMonths[i]
+	print(yr, mo, "->", end="")
+	result = daysInMonth(yr, mo)
+	if result == testResults[i]:
+		print("OK")
+	else:
+		print("Failed")
